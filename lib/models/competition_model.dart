e057870685e98546c825c994c5fec1b338f4fc56
Marco Rasp <marco.rasp@gmail.com>
2020-03-23 23:48:39 +0100

    added matchdays

--- lib/models/competition_model.dart
+++ lib/models/competition_model.dart
@@ -1,9 +1,15 @@
 import 'package:FlutterFootball/models/team_model.dart';
 
 class CompetitionModel {
+  final int id;
   final String name;
   final String logoUrl;
   final List<TeamModel> teams;
 
-  CompetitionModel({this.name, this.logoUrl, this.teams});
+  CompetitionModel({this.id, this.name, this.logoUrl, this.teams});
 }
+
+List<CompetitionModel> dummyCompetitions = [
+  new CompetitionModel(id: 1, name: "Bundesliga", teams: null),
+  new CompetitionModel(id: 2, name: "Premier League", teams: null),
+];
--- lib/screens/matches_screen.dart
+++ lib/screens/matches_screen.dart
@@ -1,6 +1,10 @@
 import 'dart:math';
 
+import 'package:FlutterFootball/models/competition_model.dart';
+import 'package:FlutterFootball/models/day_competiition_matches.dart';
+import 'package:FlutterFootball/models/day_model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../models/score_model.dart';
 import '../models/team_model.dart';
 import '../models/match_model.dart';
@@ -14,11 +18,11 @@ class MatchesScreen extends StatefulWidget {
 }
 
 class MatchesScreenState extends State<MatchesScreen> {
-  List<MatchModel> getMatches() {
+  List<MatchModel> getMatches(int count) {
     var numGenerator = new Random();
     List<MatchModel> matches = [];
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < count; i++) {
       var homeTeamIndex = numGenerator.nextInt(dummyTeams.length);
       var awayTeamIndex;
       do {
@@ -38,20 +42,92 @@ class MatchesScreenState extends State<MatchesScreen> {
     return matches;
   }
 
+  List<DayCompetitionMatchesModel> getDayCompetitionMatches(DateTime date) {
+    List<DayCompetitionMatchesModel> dayCompetitionMatches = [];
+
+    dayCompetitionMatches.add(new DayCompetitionMatchesModel(
+        date: date, competition: dummyCompetitions[0], matchDayName: "", matches: getMatches(5)));
+
+    return dayCompetitionMatches;
+  }
+
+  List<DayModel> getDays(int count) {
+    List<DayModel> days = [];
+
+    for (var i = 1; i < count + 1; i++) {
+      var date = DateTime.now().add(new Duration(days: i * 7));
+
+      days.add(
+        new DayModel(
+          date: date,
+          dayCompetitionsMatches: getDayCompetitionMatches(date),
+        ),
+      );
+    }
+
+    return days;
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<MatchModel> matches = getMatches();
+//    List<MatchModel> matches = getMatches(10);
+    List<DayModel> days = getDays(3);
 
     return new ListView.builder(
-      itemCount: matches.length,
+      itemCount: days.length,
       itemBuilder: (context, i) => new Column(
         children: <Widget>[
-          new Divider(
+          Divider(
             height: 5.0,
           ),
-          new MatchCard(matches[i])
+          Container(
+              child: Text(
+            DateFormat('EEEE, MMMM d').format(days[i].date),
+            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          )),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: days[i].dayCompetitionsMatches.length,
+            itemBuilder: (context, j) => new Column(
+              children: <Widget>[
+                new Divider(
+                  height: 5.0,
+                ),
+                Container(
+                    child: Text(
+                  days[i].dayCompetitionsMatches[j].competition.name,
+                  textAlign: TextAlign.start,
+                  style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                )),
+                new ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: days[i].dayCompetitionsMatches[j].matches.length,
+                  itemBuilder: (context, k) => new Column(
+                    children: <Widget>[
+                      new Divider(
+                        height: 5.0,
+                      ),
+                      new MatchCard(days[i].dayCompetitionsMatches[j].matches[k])
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
+
+//    return new ListView.builder(
+//      itemCount: matches.length,
+//      itemBuilder: (context, i) => new Column(
+//        children: <Widget>[
+//          new Divider(
+//            height: 5.0,
+//          ),
+//          new MatchCard(matches[i])
+//        ],
+//      ),
+//    );
   }
 }
